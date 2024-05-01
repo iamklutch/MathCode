689e54fe20fb6916cce0c3b5e319ca7660ea9c5e
JamesH <yuki@yukidev.com>
2024-04-30 20:56:14 -0600

    Adding empty files as placeholders

---
+++ interchangeList.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""Given a list, write a Python program to swap first and last element of the list.
+
+Examples: 
+
+Input : [12, 35, 9, 56, 24]
+Output : [24, 35, 9, 56, 12]
+
+Input : [1, 2, 3]
+Output : [3, 2, 1]"""
+
+
---
+++ swapList.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""Given a list in Python and provided the positions of the elements, write a program to swap the two elements in the list. 
+
+Examples:  
+
+    Input : List = [23, 65, 19, 90], pos1 = 1, pos2 = 3
+    Output : [19, 65, 23, 90]
+
+    Input : List = [1, 2, 3, 4, 5], pos1 = 2, pos2 = 5
+    Output : [1, 5, 3, 4, 2]"""
+
+
