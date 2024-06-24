88823f2865bf53ad0d7ce6d35510c5b7383dc6ee
JamesH <yuki@yukidev.com>
2024-06-24 09:41:10 -0600

    Completed, need to refactor and simplify

--- swapList.py
+++ swapList.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-"""Given a list in Python and provided the positions of the elements, write a program to swap the two elements in the list. 
+"""Given a list in Python and provided the positions of the elements
+, write a program to swap the two elements in the list. 
 
 Examples:  
 
@@ -9,5 +10,23 @@ Examples:
 
     Input : List = [1, 2, 3, 4, 5], pos1 = 2, pos2 = 5
     Output : [1, 5, 3, 4, 2]"""
+
+list1 = [23, 65, 19, 90]
+pos1 = 1
+pos2 = 3
+
+def swap_list(the_list, pos1, pos2):
+
+    index_1 = pos1 - 1
+    index_2 = pos2 - 1
+    list_item_1 = the_list[index_1]
+    list_item_2 = the_list[index_2]
+    
+    the_list[index_1] = list_item_2
+    the_list[index_2] = list_item_1
+
+    return (the_list)
+
+print(swap_list(list1, pos1, pos2))
 
 
